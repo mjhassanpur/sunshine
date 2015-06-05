91d02f329321389f885a9695672ccaddadd6a41f
mjhassanpur <mjhassanpur@gmail.com>
2015-06-04 21:47:00 -0700

    Get location from shared preferences

--- app/src/main/java/com/github/mjhassanpur/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/github/mjhassanpur/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.github.mjhassanpur.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -59,7 +61,10 @@ public class ForecastFragment extends Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
-            new FetchWeatherTask().execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            new FetchWeatherTask().execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
