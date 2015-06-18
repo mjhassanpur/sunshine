77c1b42b4648c36c2fccb1c28d8a162545630435
mjhassanpur <mjhassanpur@gmail.com>
2015-06-18 15:32:41 -0700

    Add weather with location to uribuilder

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -131,11 +131,9 @@ public class WeatherContract {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
 
-        /*
-            Student: Fill in this buildWeatherLocation function
-         */
+        /* Student: This is the buildWeatherLocation function you filled in. */
         public static Uri buildWeatherLocation(String locationSetting) {
-            return null;
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
         }
 
         public static Uri buildWeatherLocationWithStartDate(
